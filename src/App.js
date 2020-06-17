db4abbfa6543e36005c0f0d5aa6522b964501478
PraSoonGosWami <goswami.prasoon555@gmail.com>
2020-06-17 11:02:58 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Paper from "@material-ui/core/Paper";
 import MainComponent from "./Components/MainComponent/MainComponent";
 import ReactGA from 'react-ga';
 import {SnackBarProvider} from "./Context/SnackBarContext";
+import Axios from "axios";
+import {Helmet} from "react-helmet";
+import ProgressBar from "./UI/ProgressBar/ProgressBar";
 
 
 const App = () => {
@@ -19,44 +22,62 @@ const App = () => {
         ReactGA.pageview('/about');
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         initializeReactGA()
-    },[])
+    }, [])
 
     const [darkMode, setDarkMode] = useState(true)
     const [dropDownValue, setDropDownValue] = useState("TT")
     const darkTheme = createMuiTheme(DarkTheme)
     const lightTheme = createMuiTheme(LightTheme)
 
+    const [token, setToken] = useState(null)
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setLoading(true)
+        Axios.get("https://covid-zones.firebaseio.com/token.json")
+            .then(res => {
+                setToken(res.data)
+            })
+            .catch(err => {
+                setToken(null)
+            })
+            .finally(() => setLoading(false))
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         const dark = localStorage.getItem("dark")
         const stateCode = localStorage.getItem("stateCode")
         dark === "Y" ? setDarkMode(true) : setDarkMode(false)
-        setDropDownValue(stateCode?stateCode:"TT")
-    },[])
+        setDropDownValue(stateCode ? stateCode : "TT")
+    }, [])
 
-    const handleDarkMode = () =>{
+    const handleDarkMode = () => {
         setDarkMode(prevState => !prevState)
-        localStorage.setItem("dark",darkMode?"N":"Y")
+        localStorage.setItem("dark", darkMode ? "N" : "Y")
     }
 
     const onDropDownChangeHandler = (event) => {
         setDropDownValue(event.target.value)
-        localStorage.setItem("stateCode",event.target.value)
+        localStorage.setItem("stateCode", event.target.value)
     }
     return (
         <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
             <SnackBarProvider>
-                <Paper style={{minHeight:"100vh"}}>
+                <Paper style={{minHeight: "100vh"}}>
                     <Appbar
                         checked={darkMode}
-                        handleChange={()=> handleDarkMode()}
+                        handleChange={() => handleDarkMode()}
                         dropDownValue={dropDownValue}
                         onDropDownChangeHandler={onDropDownChangeHandler}/>
-                    <MainComponent stateCode={dropDownValue}/>
+                    {!loading && <MainComponent stateCode={dropDownValue}/>}
+                    {loading && <ProgressBar/>}
                 </Paper>
             </SnackBarProvider>
+            {token && <Helmet>
+                <script
+                    src={"https://apis.mapmyindia.com/advancedmaps/v1/" + token + "/map_load?v=1.5&plugins=coronaLayers&callback=initializeMap"}></script>
+            </Helmet>}
         </ThemeProvider>
 
     );
